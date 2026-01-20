7d84da4b8d3cb7a098afd89dc841564b6e680155
Dilojan R. <dilodilojan662000@gmail.com>
2026-01-20 13:00:30 +0530

    server component base file added - initial state

---
+++ servercomp/src/main/java/com/eztrad/servercomp/ServercompApplication.java
@@ -0,0 +1,13 @@
+package com.eztrad.servercomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServercompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServercompApplication.class, args);
+	}
+
+}
---
+++ servercomp/src/test/java/com/eztrad/servercomp/ServercompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eztrad.servercomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServercompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
