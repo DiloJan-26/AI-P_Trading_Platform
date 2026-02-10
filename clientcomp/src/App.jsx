458a16e0a0d45c0b333f18b99832ed9b97435c98
Dilojan R. <dilodilojan662000@gmail.com>
2026-02-10 19:40:18 +0530

    stopped at 18:00

--- clientcomp/src/App.jsx
+++ clientcomp/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-import { Button } from './components/ui/button'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button>Click me</Button>
+      <Navbar />
     </>
   )
 }
